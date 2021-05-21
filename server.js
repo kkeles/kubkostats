8bec8b04ef8687c6506003c07f37467f08cb000d
kkeles <kkelesh@gmail.com>
2021-05-21 11:56:33 +0200

    Made changes following Brad to prepare for Heroku

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
+const path = require('path');
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ extended: false }));
 
 mongoose.connect('mongodb+srv://kkUser:sanchez@kubus.skllo.mongodb.net/kubkoDB', {
 	useNewUrlParser: true,
@@ -14,8 +15,16 @@ mongoose.connect('mongodb+srv://kkUser:sanchez@kubus.skllo.mongodb.net/kubkoDB',
 app.use('/', require('./routes/foodRoute'));
 app.use('/', require('./routes/walkRoute'));
 
-const port = process.env.PORT || 4005;
+// Serve static assets
+if (process.env.NODE_ENV === 'production') {
+	// to set static folder
+	app.use(express.static('frontend/build'));
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve('frontend', 'build', 'index.html'));
+	});
+}
 
+const port = process.env.PORT || 4005;
 app.listen(port, () => {
 	console.log(`Server running on port ${port}`);
 });
