b62c9deb90e0912e5071036a3b5a6cb07f0ef4f2
kkeles <kkelesh@gmail.com>
2021-05-21 17:50:29 +0200

    retry with dotenv

--- config/db.js
+++ config/db.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 const config = require('config');
-const MONGO_KEY = config.get('MONGO_KEY');
+// const MONGO_KEY = config.get('MONGO_KEY');
+require('dotenv').config();
 
 const connectDB = async () => {
 	try {
-		await mongoose.connect(`mongodb+srv://kkUser:${MONGO_KEY}@kubus.skllo.mongodb.net/kubkoDB`, {
+		await mongoose.connect(`mongodb+srv://kkUser:${process.env.MONGO_KEY}@kubus.skllo.mongodb.net/kubkoDB`, {
 			useNewUrlParser: true,
 			useUnifiedTopology: true
 		});
