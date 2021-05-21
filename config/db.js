b0a5209b5365295d40686b8a42729c96cce0821d
kkeles <kkelesh@gmail.com>
2021-05-21 18:01:05 +0200

    clear dotenv

--- config/db.js
+++ config/db.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
 const config = require('config');
-// const MONGO_KEY = config.get('MONGO_KEY');
-require('dotenv').config();
+const MONGO_KEY = config.get('MONGO_KEY');
 
 const connectDB = async () => {
 	try {
-		await mongoose.connect(`mongodb+srv://kkUser:${process.env.MONGO_KEY}@kubus.skllo.mongodb.net/kubkoDB`, {
+		await mongoose.connect(`mongodb+srv://kkUser:${MONGO_KEY}@kubus.skllo.mongodb.net/kubkoDB`, {
 			useNewUrlParser: true,
 			useUnifiedTopology: true
 		});
